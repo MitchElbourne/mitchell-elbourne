399e2642f5ca04b333cf52624313cd619767732b
Mitchelbourne <mitchell.elbourne@agency51.com>
2022-03-27 13:28:21 +0100

    Separator styling

--- header.php
+++ header.php
@@ -8,26 +8,20 @@
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<?php
-    $navItems = ["Projects", "Contact me"]
-?>
+    <?php wp_body_open(); ?>
+    <?php
+        $navItems = ["Projects", "Contact me"]
+    ?>
+
 	<header>
-		<div class="logo">
+		<a class="logo" href="/">
             <span class="h2 code">&lt;M&gt;</span>      
-        </div>
+        </a>
 
         <ul>
-            <?php foreach ($navItems as $index => $item) { ?>
+            <?php foreach ($navItems as $item) { ?>
                 <li class="code">
-                    <?php
-                    // echo 
-                    // '<a href="/'. $item .'">
-                    //     <span class="caret">&lt;0'. $index .'></span>
-                    //     '. $item .'
-                    //     <span class="caret">&lt;/0'. $index .'&gt;</span>
-                    // </a>';
-                    echo 
+                    <?php echo 
                     '<a href="/'. $item .'">
                         <span class="caret">&lt;</span><span class="nav-icon">/</span>'.$item.'<span class="caret">&gt;</span>
                     </a>';
